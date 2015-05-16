d0e3ef2f6de7d30496642b494371e69b477e24c6
Drew Carey Buglione <drew@dangerousthings.com>
2015-05-16 01:08:25 -0400

    Improve ScanActivity

--- src/com/switcherooboard/android/ScanActivity.java
+++ src/com/switcherooboard/android/ScanActivity.java
@@ -4,81 +4,158 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
-public class ScanActivity extends Activity implements BluetoothAdapter.LeScanCallback {
+import java.util.ArrayList;
 
-  private static final String TAG = "Switcheroo.ScanActivity";
+public class ScanActivity extends Activity implements OnItemClickListener {
 
-  public static final String EXTRA_SWITCHEROO = "com.switcherooboard.android.extra.SWITCHEROO";
+  private static final String TAG = "ScanActivity";
 
-  BluetoothAdapter mBluetoothAdapter;
+  private BluetoothAdapter mBluetoothAdapter;
 
-  GridView mGridView;
+  private GridView mGridView;
+
+  private SwitcherooScan mSwitcherooScan;
+
+  private ArrayAdapter mArrayAdapter;
+
+  /* Activity */
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.scan);
+    this.setContentView(R.layout.scan);
 
-    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-  }
+    this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-  @Override
-  public void onStart() {
-    super.onStart();
+    this.mSwitcherooScan = (SwitcherooScan) this.getLastNonConfigurationInstance();
 
-    mGridView = (GridView) findViewById(R.id.gridview);
+    if (this.mSwitcherooScan == null) {
+      this.mSwitcherooScan = new SwitcherooScan();
+      this.mBluetoothAdapter.startLeScan(this.mSwitcherooScan);
+    }
 
-    mGridView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1));
+    this.mSwitcherooScan.setDataSetObserver(new DataSetObserver() {
+      @Override
+      public void onChanged() {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+          public void run() {
+            ScanActivity.this.mArrayAdapter.notifyDataSetChanged();
+          }
+        });
+      }
 
-    mGridView.setOnItemClickListener(new OnItemClickListener() {
-      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(ScanActivity.this, MainActivity.class);
-
-        final BluetoothDevice device = (BluetoothDevice) parent.getAdapter().getItem(position);
-        intent.putExtra(ScanActivity.EXTRA_SWITCHEROO, new GattSwitcheroo(device.getAddress()));
-
-        startActivity(intent);
+      @Override
+      public void onInvalidated() {
       }
     });
+
+    this.mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.mSwitcherooScan.getResultsList());
+
+    this.mGridView = (GridView) this.findViewById(R.id.gridview);
+
+    this.mGridView.setAdapter(this.mArrayAdapter);
+
+    this.mGridView.setOnItemClickListener(this);
   }
 
   @Override
-  public void onResume() {
-    super.onResume();
-
-    android.util.Log.d(TAG, "startLeScan()");
-    mBluetoothAdapter.startLeScan(this);
-  }
-
-  @Override
-  public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-    android.util.Log.d(TAG, "onLeScan" + " -> " + "\"" + device.getName() + "\"");
-
-    if (device.getName().equals("Switcheroo")) {
-      final ArrayAdapter adapter = (ArrayAdapter) mGridView.getAdapter();
-
-      runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          if (adapter.getPosition(device) == -1) adapter.add(device);
-        }
-      });
-    }
+  public Object onRetainNonConfigurationInstance() {
+    this.mSwitcherooScan.setDataSetObserver(null);
+    return this.mSwitcherooScan;
   }
 
   @Override
   public void onPause() {
     super.onPause();
 
-    android.util.Log.d(TAG, "stopLeScan()");
-    mBluetoothAdapter.stopLeScan(this);
+    if (this.isFinishing()) {
+      this.mBluetoothAdapter.stopLeScan(this.mSwitcherooScan);
+    }
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+
+    if (this.isFinishing()) {
+      this.mBluetoothAdapter.stopLeScan(this.mSwitcherooScan);
+    }
+  }
+
+  /* */
+
+  public static final String EXTRA_SWITCHEROO = "com.switcherooboard.android.extra.SWITCHEROO";
+
+  /* OnItemClickListener */
+
+  @Override
+  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    Intent intent = new Intent(ScanActivity.this, MainActivity.class);
+
+    final BluetoothDevice device = (BluetoothDevice) parent.getAdapter().getItem(position);
+    intent.putExtra(ScanActivity.EXTRA_SWITCHEROO, new GattSwitcheroo(device.getAddress()));
+
+    this.startActivity(intent);
+  }
+
+  /* */
+
+  private static class SwitcherooScan implements BluetoothAdapter.LeScanCallback {
+
+    private final ArrayList<BluetoothDevice> mResultsList = new ArrayList<BluetoothDevice>();
+
+    private DataSetObserver mDataSetObserver;
+
+    private Handler mHandler;
+
+    public SwitcherooScan() {
+      new Thread(new Runnable() {
+        public void run() {
+          Looper.prepare();
+          SwitcherooScan.this.mHandler = new Handler();
+          Looper.loop();
+        }
+      }).start();
+    }
+
+    public ArrayList<BluetoothDevice> getResultsList() {
+      return mResultsList;
+    }
+
+    public void setDataSetObserver(DataSetObserver observer) {
+      this.mDataSetObserver = observer;
+    }
+
+    @Override
+    public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+      android.util.Log.d(TAG, "onLeScan" + " -> " + "\"" + device.getName() + "\"");
+
+      if (device.getName().equals("Switcheroo")) {
+        this.mHandler.post(new Runnable() {
+          @Override
+          public void run() {
+            if (!SwitcherooScan.this.mResultsList.contains(device)) {
+              SwitcherooScan.this.mResultsList.add(device);
+
+              if (SwitcherooScan.this.mDataSetObserver != null) {
+                SwitcherooScan.this.mDataSetObserver.onChanged();
+              }
+            }
+          }
+        });
+      }
+    }
   }
 
 }
